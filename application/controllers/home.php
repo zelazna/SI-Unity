ec31a3f2b3aa75b1df49def2e748f3028d74fe93
constantin guidon <constantin@MacBook-Pro-de-constantin.local>
2016-04-13 05:51:24 +0200

    Version 0.7

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,32 +1,30 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-session_start(); //we need to call PHP's session object to access it through CI
-class Home extends CI_Controller {
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
 
     function __construct()
     {
         parent::__construct();
+        $this->load->library('session');
     }
 
-    function index()
-    {
-        if($this->session->userdata('logged_in'))
-        {
-            $session_data = $this->session->userdata('logged_in');
-            $data['username'] = $session_data['username'];
-            $this->load->view('home_view', $data);
-        }
-        else
-        {
-            //If no session, redirect to login page
-            redirect('login', 'refresh');
-        }
-    }
+//    function index()
+//    {
+//        if ($this->session->has_userdata("username")) {
+//            $session_data = $this->session->userdata('logged_in');
+//            $data['username'] = $session_data['username'];
+//            $this->load->view('index.php', $data);
+//        } else {
+//            //If no session, redirect to login page
+//            redirect('index.php/connexion/index', 'refresh');
+//        }
+//    }
 
     function logout()
     {
-        $this->session->unset_userdata('logged_in');
-        session_destroy();
-        redirect('home', 'refresh');
+        $this->session->sess_destroy();
+        redirect('pages/view', 'refresh');
     }
 
 }
