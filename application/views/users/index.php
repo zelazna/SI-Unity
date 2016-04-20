5ea08991749ca7a25e4beb7cd1d3c96ea81be022
constantin guidon <constantin@MacBook-Pro-de-constantin.local>
2016-04-20 14:38:57 +0200

    last commit

--- application/views/users/index.php
+++ application/views/users/index.php
@@ -1,14 +1,3 @@
-<!--<h2>--><?php //echo $title; ?><!--</h2>-->
-<!---->
-<!--<table>-->
-<!--    --><?php
-//    foreach ($users as $users_details): ?>
-<!--        <tr>-->
-<!--            <td>--><?php //echo $users_details['score']; ?><!--</td>-->
-<!--            <td class="main">--><?php //echo $users_details['username']; ?><!--</td>-->
-<!--            <td><a href="--><?php //echo site_url('users/' . $users_details['username']); ?><!--">View Player</a></td>-->
-<!--        </tr>-->
-<!--    --><?php //endforeach; ?>
 <!--[if lt IE 7]>
 <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>
